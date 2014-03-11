2c087fcff000bb2980d6c99e1f7ba96f3bdcacaf
ss <ss>
2014-03-11 10:45:33 +0800

    矩阵求逆，初步实现

--- NVIDIA_CUDA-5.0_Samples/8_diy/matrixInv/matrixInverse.cu
+++ NVIDIA_CUDA-5.0_Samples/8_diy/matrixInv/matrixInverse.cu
@@ -10,9 +10,10 @@
 //
 ////////////////////////////////////////////////////////////////////////////
 
-/* Template project which demonstrates the basics on how to setup a project
-* example application.
-* Host code.
+/* Inverse Matrix by CUBLAS library
+8.3. cublas<t>getrfBatched(): http://docs.nvidia.com/cuda/cublas/index.html#cublas-lt-t-gt-getrfbatched  in cuda5.0
+8.4. cublas<t>getriBatched(): http://docs.nvidia.com/cuda/cublas/index.html#cublas-lt-t-gt-getribatched  in cuda5.5
+
 */
 
 // includes, system
@@ -24,9 +25,36 @@
 // includes CUDA
 #include <cuda_runtime.h>
 
+// includes CUDA BLAS
+#include <cublas_v2.h>
+
 // includes, project
 #include <helper_cuda.h>
 #include <helper_functions.h> // helper functions for SDK examples
+
+#define BENCH_MATRIX_EXP			7 //2~10
+#define BENCH_MATRIX_ROWS           (1<<BENCH_MATRIX_EXP)
+#define CUBLAS_TEST_COUNT			(10) // 10~1000
+
+#define T_ELEM	float
+//#define T_ELEM	double
+
+
+__inline__ __device__ __host__  float cuGet(double x)
+{
+    return float(x);
+}
+
+void fillupMatrixDebug(T_ELEM *A , int size )
+{
+    for (int j = 0; j < size; j++)
+    {
+        for (int i = 0; i < size; i++)
+        {
+            A[i + size*j ] = cuGet(i + j);
+        }
+    }
+}
 
 ////////////////////////////////////////////////////////////////////////////////
 // declaration, forward
@@ -80,82 +108,126 @@ void
 runTest(int argc, char **argv)
 {
     bool bTestResult = true;
+	cudaError_t err1;
 
     printf("%s Starting...\n\n", argv[0]);
 
     // use command-line specified CUDA device, otherwise use device with highest Gflops/s
     int devID = findCudaDevice(argc, (const char **)argv);
 
+	// size
+	int matrixRows = BENCH_MATRIX_ROWS;
+    int matrixSize = matrixRows * matrixRows;
+
+	// data
+	T_ELEM *A = NULL;
+    T_ELEM **devPtrA = 0;
+    T_ELEM **devPtrA_dev = NULL;
+
+	T_ELEM **devPtrC = 0;
+    T_ELEM **devPtrC_dev = NULL;
+
+	// matrix A, input matrix
+    devPtrA =(T_ELEM **)malloc( CUBLAS_TEST_COUNT * sizeof(T_ELEM));
+	for (int i = 0; i < CUBLAS_TEST_COUNT ; i++)
+    {
+        err1 = cudaMalloc((void **)&devPtrA[i], matrixSize * sizeof(T_ELEM));
+	}
+
+    err1 = cudaMalloc((void **)&devPtrA_dev, CUBLAS_TEST_COUNT * sizeof(T_ELEM));
+    err1 = cudaMemcpy(devPtrA_dev, devPtrA, CUBLAS_TEST_COUNT * sizeof(*devPtrA), cudaMemcpyHostToDevice);
+
+    A  = (T_ELEM *)malloc(matrixSize * sizeof(T_ELEM));
+
+	memset(A, 0xFF, matrixSize * sizeof(A[0]));
+	fillupMatrixDebug( A, matrixRows );
+
+	for (int i = 0; i < CUBLAS_TEST_COUNT ; i++)
+    {
+		cublasSetMatrix( matrixRows, matrixRows, sizeof(A[0]), A, matrixRows, devPtrA[i], matrixRows);
+	}
+
+	// matrix C, output inverse matrix of A
+	devPtrC =(T_ELEM **)malloc( CUBLAS_TEST_COUNT * sizeof(T_ELEM));
+	for (int i = 0; i < CUBLAS_TEST_COUNT ; i++)
+    {
+        err1 = cudaMalloc((void **)&devPtrC[i], matrixSize * sizeof(T_ELEM));
+	}
+
+    err1 = cudaMalloc((void **)&devPtrC_dev, CUBLAS_TEST_COUNT * sizeof(T_ELEM));
+    err1 = cudaMemcpy(devPtrC_dev, devPtrC, CUBLAS_TEST_COUNT * sizeof(*devPtrC), cudaMemcpyHostToDevice);
+
+	// temp data
+	int *pivotArray = new int[ matrixRows*CUBLAS_TEST_COUNT ];
+	int *infoArray = new int[ CUBLAS_TEST_COUNT ];
+
+	// blas config
+    cublasHandle_t handle;
+	cublasCreate(&handle);
+
+	// timer
     StopWatchInterface *timer = 0;
     sdkCreateTimer(&timer);
     sdkStartTimer(&timer);
 
-    unsigned int num_threads = 32;
-    unsigned int mem_size = sizeof(float) * num_threads;
+    cublasSetStream(handle, 0);
+	
+	cublasStatus_t status ;
 
-    // allocate host memory
-    float *h_idata = (float *) malloc(mem_size);
+	// LU factorization
+	status = cublasSgetrfBatched(handle, 
+		matrixRows, 
+		devPtrA_dev, 
+		matrixRows,
+		pivotArray,
+		infoArray,
+		CUBLAS_TEST_COUNT);
 
-    // initalize the memory
-    for (unsigned int i = 0; i < num_threads; ++i)
+	if (status != CUBLAS_STATUS_SUCCESS)
     {
-        h_idata[i] = (float) i;
+          cudaError_t cuError = cudaGetLastError();
+          fprintf(stderr, "!!!! GPU program execution error : cublas Error=%d, cuda Error=%d,(%s)\n", status, cuError,cudaGetErrorString(cuError));
+          return ;
     }
 
-    // allocate device memory
-    float *d_idata;
-    checkCudaErrors(cudaMalloc((void **) &d_idata, mem_size));
-    // copy host memory to device
-    checkCudaErrors(cudaMemcpy(d_idata, h_idata, mem_size,
-                               cudaMemcpyHostToDevice));
+	// inversion of matrices A, output result to matrices C
+	status = cublasSgetriBatched(handle, 
+		matrixRows, 
+		devPtrA_dev, 
+		matrixRows,
+		pivotArray,
+		devPtrC_dev,
+		matrixRows,
+		infoArray,
+		CUBLAS_TEST_COUNT);
 
-    // allocate device memory for result
-    float *d_odata;
-    checkCudaErrors(cudaMalloc((void **) &d_odata, mem_size));
+	if (status != CUBLAS_STATUS_SUCCESS)
+    {
+          cudaError_t cuError = cudaGetLastError();
+          fprintf(stderr, "!!!! GPU program execution error : cublas Error=%d, cuda Error=%d,(%s)\n", status, cuError,cudaGetErrorString(cuError));
+          return ;
+    }
 
-    // setup execution parameters
-    dim3  grid(1, 1, 1);
-    dim3  threads(num_threads, 1, 1);
-
-    // execute the kernel
-    testKernel<<< grid, threads, mem_size >>>(d_idata, d_odata);
-
-    // check if kernel execution generated and error
-    getLastCudaError("Kernel execution failed");
-
-    // allocate mem for the result on host side
-    float *h_odata = (float *) malloc(mem_size);
-    // copy result from device to host
-    checkCudaErrors(cudaMemcpy(h_odata, d_odata, sizeof(float) * num_threads,
-                               cudaMemcpyDeviceToHost));
 
     sdkStopTimer(&timer);
     printf("Processing time: %f (ms)\n", sdkGetTimerValue(&timer));
     sdkDeleteTimer(&timer);
 
-    // compute reference solution
-    float *reference = (float *) malloc(mem_size);
-    computeGold(reference, h_idata, num_threads);
 
-    // check result
-    if (checkCmdLineFlag(argc, (const char **) argv, "regression"))
-    {
-        // write file for regression test
-        sdkWriteFile("./data/regression.dat", h_odata, num_threads, 0.0f, false);
-    }
-    else
-    {
-        // custom output handling when no regression test running
-        // in this case check if the result is equivalent to the expected soluion
-        bTestResult = compareData(reference, h_odata, num_threads, 0.0f, 0.0f);
-    }
 
     // cleanup memory
-    free(h_idata);
-    free(h_odata);
-    free(reference);
-    checkCudaErrors(cudaFree(d_idata));
-    checkCudaErrors(cudaFree(d_odata));
+	if (A) free (A); 
+
+	for(int i = 0; i < CUBLAS_TEST_COUNT; ++i) {       
+            if(devPtrA[i]) cudaFree(devPtrA[i]);
+            if(devPtrC[i]) cudaFree(devPtrC[i]);
+        }  
+
+	if (devPtrA) free(devPtrA);           
+	if (devPtrC) free(devPtrC); 
+
+	if (devPtrA_dev) cudaFree(devPtrA_dev);
+	if (devPtrC_dev) cudaFree(devPtrC_dev); 
 
     cudaDeviceReset();
     exit(bTestResult ? EXIT_SUCCESS : EXIT_FAILURE);
