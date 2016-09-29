bf5e545f0f2905faf0893cc552f8c139076ee986
ss <ss>
2016-09-29 11:12:06 +0800

    add driver box filter

---
+++ NVIDIA_CUDA-5.0_Samples/8_diy/driverBoxFilter/boxFilter.cu
@@ -0,0 +1,308 @@
+
+#include "boxFilter.cuh"
+#include <cuda_runtime.h>
+
+#define		BLOCK_SIZE_1D	64	// BLOCK_SIZE = BLOCK_SIZE_1D
+#define		USE_TEXTURE		0
+#define		USE_SDK			1
+
+// for gtx480	(USE_TEXTURE,USE_SDK) = (0,0)
+// for tk1		(USE_TEXTURE,USE_SDK) = (1,1)
+
+#if !USE_SDK
+
+__global__ void scan_kernel( float *imDst_C,float * imSrc_C, int nElement, int nBlockStride, int nThreadStride )
+{
+	__shared__ float sdata[BLOCK_SIZE_1D*2];//blockDim.x*2
+	int offset = 0;
+	for(  ; offset < nElement ; offset += blockDim.x )
+	{
+		int length = blockDim.x;
+		if( offset + blockDim.x > nElement )
+		{
+			length = nElement % blockDim.x;
+		}
+
+		if( threadIdx.x+offset >= nElement )
+			return;
+
+		sdata[threadIdx.x] = imSrc_C[(threadIdx.x+offset)*nThreadStride + blockIdx.x*nBlockStride ];
+
+		if( offset!=0 && threadIdx.x == 0 )
+			sdata[0] += imDst_C[(threadIdx.x+offset-1)*nThreadStride + blockIdx.x*nBlockStride ];
+
+		__syncthreads();
+
+		int first = 0;
+
+		for ( int d=1;d<length; d+=d, first=blockDim.x-first )
+		{
+			if( threadIdx.x < d )
+				sdata[threadIdx.x+blockDim.x-first] = sdata[threadIdx.x+first];
+			else
+				sdata[threadIdx.x+blockDim.x-first] = sdata[threadIdx.x+first] + sdata[threadIdx.x-d+first];
+			__syncthreads();
+		}
+
+		imDst_C[(threadIdx.x+offset)*nThreadStride + blockIdx.x*nBlockStride ] = sdata[threadIdx.x+first];
+		__syncthreads();
+
+	}
+}
+
+__global__ void delta( float *imDst_C,float *imCum_C,int r, int nElement, int nBlockStride, int nThreadStride)
+{
+	if( threadIdx.x > nElement )
+		return;
+
+	for( int nCurrentIndex = threadIdx.x;nCurrentIndex< nElement; nCurrentIndex+=blockDim.x )
+	{
+		int nCurrentElement = nCurrentIndex*nThreadStride + blockIdx.x*nBlockStride;
+
+		if( nCurrentIndex < r + 1 )
+			imDst_C[ nCurrentElement ] =	imCum_C[ nCurrentElement + r*nThreadStride ];
+		else if ( nCurrentIndex >= r + 1 && nCurrentIndex < nElement-r )
+			imDst_C[ nCurrentElement ] =	imCum_C[ nCurrentElement + r*nThreadStride ] -  
+															imCum_C[ nCurrentElement - (r + 1)*nThreadStride ] ;
+		else //if ( nCurrentIndex >= height-r && nCurrentIndex < height )
+			imDst_C[ nCurrentElement ] =	imCum_C[ (nElement-1)*nThreadStride + blockIdx.x*nBlockStride ] -  
+															imCum_C[ nCurrentElement - (r + 1)*nThreadStride ] ;
+	}
+
+}
+
+//boxfilter
+/*%   BOXFILTER   O(1) time box filtering using cumulative sum
+%
+%   - Definition imDst(x, y)=sum(sum(imSrc(x-r:x+r,y-r:y+r)));
+%   - Running time independent of r; 
+%   - Equivalent to the function: colfilt(imSrc, [2*r+1, 2*r+1], 'sliding', @sum);
+%   - But much faster.*/
+void boxfilter(float *imSrc,float *imCum_C,float *imDst,int r,int height,int width)
+{
+	
+	int nBlockSize;
+
+	// 垂直Y方向累加，半径r，即2*r+1个数累加
+	nBlockSize = height>BLOCK_SIZE_1D? BLOCK_SIZE_1D:  height;
+
+	scan_kernel<<<width, nBlockSize >>>( imCum_C,imSrc, height, 1, width);// 垂直Y方向累加
+
+	delta<<<width,nBlockSize>>>( imDst,imCum_C,r, height, 1, width ); // 垂直Y方向等距离相减
+
+	// 水平X方向累加，半径r，即2*r+1个数累加
+	nBlockSize = width>BLOCK_SIZE_1D? BLOCK_SIZE_1D: width ;
+
+	scan_kernel<<<height, nBlockSize >>>( imCum_C,imDst, width, width, 1);// 水平X方向累加
+
+	delta<<<height,nBlockSize>>>( imDst,imCum_C,r, width, width, 1 ); // 水平X方向等距离相减
+}
+
+void initTexture(int width, int height){}
+
+void freeTextures(){}
+
+#else
+
+#if USE_TEXTURE
+texture<float, 2> tex;
+cudaArray *d_array;
+
+void initTexture(int width, int height)
+{
+    int size = width * height * sizeof(float);
+
+    // copy image data to array
+    cudaChannelFormatDesc channelDesc;
+	channelDesc = cudaCreateChannelDesc(32, 0, 0, 0, cudaChannelFormatKindFloat);
+    
+    cudaMallocArray(&d_array, &channelDesc, width, height);
+    //cudaMemcpyToArray(d_array, 0, 0, pImage, size, cudaMemcpyHostToDevice);
+
+    // set texture parameters
+    tex.addressMode[0] = cudaAddressModeClamp;
+    tex.addressMode[1] = cudaAddressModeClamp;
+    tex.filterMode = cudaFilterModePoint;
+    tex.normalized = true;
+
+    // Bind the array to the texture
+   cudaBindTextureToArray(tex, d_array, channelDesc);
+}
+
+void freeTextures()
+{
+    cudaFreeArray(d_array);
+}
+
+#else
+void initTexture(int width, int height){}
+
+void freeTextures(){}
+#endif
+
+// process row
+__device__ void
+d_boxfilter_x(float *id, float *od, int w, int h, int r)
+{
+    float scale = 1.0f / (float)((r << 1) + 1);
+
+    float t;
+    // do left edge
+    t = id[0] * r;
+
+    for (int x = 0; x < (r + 1); x++)
+    {
+        t += id[x];
+    }
+
+    od[0] = t * scale;
+
+    for (int x = 1; x < (r + 1); x++)
+    {
+        t += id[x + r];
+        t -= id[0];
+        od[x] = t * scale;
+    }
+
+    // main loop
+    for (int x = (r + 1); x < w - r; x++)
+    {
+        t += id[x + r];
+        t -= id[x - r - 1];
+        od[x] = t * scale;
+    }
+
+    // do right edge
+    for (int x = w - r; x < w; x++)
+    {
+        t += id[w - 1];
+        t -= id[x - r - 1];
+        od[x] = t * scale;
+    }
+}
+
+// process column
+__device__ void
+d_boxfilter_y(float *id, float *od, int w, int h, int r)
+{
+    float scale = 1.0f / (float)((r << 1) + 1);
+
+    float t;
+    // do left edge
+    t = id[0] * r;
+
+    for (int y = 0; y < (r + 1); y++)
+    {
+        t += id[y * w];
+    }
+
+    od[0] = t * scale;
+
+    for (int y = 1; y < (r + 1); y++)
+    {
+        t += id[(y + r) * w];
+        t -= id[0];
+        od[y * w] = t * scale;
+    }
+
+    // main loop
+    for (int y = (r + 1); y < (h - r); y++)
+    {
+        t += id[(y + r) * w];
+        t -= id[((y - r) * w) - w];
+        od[y * w] = t * scale;
+    }
+
+    // do right edge
+    for (int y = h - r; y < h; y++)
+    {
+        t += id[(h-1) * w];
+        t -= id[((y - r) * w) - w];
+        od[y * w] = t * scale;
+    }
+}
+
+__global__ void
+d_boxfilter_x_global(float *id, float *od, int w, int h, int r)
+{
+    unsigned int y = blockIdx.x*blockDim.x + threadIdx.x;
+    d_boxfilter_x(&id[y * w], &od[y * w], w, h, r);
+}
+
+__global__ void
+d_boxfilter_y_global(float *id, float *od, int w, int h, int r)
+{
+    unsigned int x = blockIdx.x*blockDim.x + threadIdx.x;
+    d_boxfilter_y(&id[x], &od[x], w, h, r);
+}
+
+#if USE_TEXTURE
+// texture version
+// texture fetches automatically clamp to edge of image
+__global__ void
+d_boxfilter_x_tex(float *od, int w, int h, int r)
+{
+    float scale = 1.0f / (float)((r << 1) + 1);
+    unsigned int y = blockIdx.x*blockDim.x + threadIdx.x;
+	if (y < h)
+    {
+    float t = 0.0f;
+
+    for (int x =- r; x <= r; x++)
+    {
+        t += tex2D(tex, x, y);
+    }
+
+    od[y * w] = t * scale;
+
+    for (int x = 1; x < w; x++)
+    {
+        t += tex2D(tex, x + r, y);
+        t -= tex2D(tex, x - r - 1, y);
+        od[y * w + x] = t * scale;
+    }
+	}
+}
+
+__global__ void
+d_boxfilter_y_tex(float *od, int w, int h, int r)
+{
+    float scale = 1.0f / (float)((r << 1) + 1);
+    unsigned int x = blockIdx.x*blockDim.x + threadIdx.x;
+
+    float t = 0.0f;
+
+    for (int y = -r; y <= r; y++)
+    {
+        t += tex2D(tex, x, y);
+    }
+
+    od[x] = t * scale;
+
+    for (int y = 1; y < h; y++)
+    {
+        t += tex2D(tex, x, y + r);
+        t -= tex2D(tex, x, y - r - 1);
+        od[y * w + x] = t * scale;
+    }
+}
+#endif
+
+void boxfilter(float *imSrc,float *imCum_C,float *imDst,int r,int height,int width)
+{
+#if USE_TEXTURE
+	cudaMemcpyToArray(d_array, 0, 0, imSrc, height*width*sizeof(float), cudaMemcpyDeviceToDevice);
+
+    cudaBindTextureToArray(tex, d_array);
+
+	d_boxfilter_x_tex<<< (height+BLOCK_SIZE_1D-1) / BLOCK_SIZE_1D, BLOCK_SIZE_1D, 0 >>>(imCum_C, width, height, r);
+#else
+
+	d_boxfilter_x_global<<< height / BLOCK_SIZE_1D, BLOCK_SIZE_1D, 0 >>>(imSrc, imCum_C, width, height, r);
+
+#endif
+	d_boxfilter_y_global<<< (width+BLOCK_SIZE_1D-1) / BLOCK_SIZE_1D, BLOCK_SIZE_1D, 0 >>>(imCum_C, imDst, width, height, r);
+
+}
+
+#endif
---
+++ NVIDIA_CUDA-5.0_Samples/8_diy/driverBoxFilter/driverBoxFilter.cu
@@ -0,0 +1,144 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <time.h>
+#include <iostream>
+//#include "timerCPP.h"
+
+using namespace std;
+
+#include <cuda_runtime.h>
+#include <thrust/host_vector.h>
+#include <thrust/device_vector.h>
+#include <thrust/sort.h>
+
+#include "boxFilter.cuh"
+
+// 最优配置
+// 对于gtx480	BLOCK_SIZE_2D = (4,128)(3.7ms pointer), (8,32)(5.2ms texture)
+// 对于gt640	BLOCK_SIZE_2D = (4,32)(40ms pointer), (4,128)(14.7ms texture)
+
+#define		USE_TEXTURE_ADDRESS	1
+#define		ENABLE_TIMER	1
+
+#if	ENABLE_TIMER
+#include <helper_timer.h>
+#endif
+
+int getMaxThreadsPerBlock()
+{
+	cudaDeviceProp deviceProp;
+	cudaGetDeviceProperties(&deviceProp, 0);
+
+	printf("\nDevice: \"%s\"\n", deviceProp.name);
+	printf("  Maximum number of threads per block:           %d\n", deviceProp.maxThreadsPerBlock);
+
+	return deviceProp.maxThreadsPerBlock;
+}
+
+class driverBoxFilter{
+public:
+	driverBoxFilter()	{ }
+	~driverBoxFilter()	{ release(); }
+	void initialize( );
+
+	void run();
+	void readFile(float **ppBuffer, std::string filename, int& width, int& height);
+	void writeFile(float *buffer, std::string filename, int width, int height=1);
+	bool verifyResult(float *p, float *p_ref, int n);
+
+private:
+	float *imCum_C, *C_ones, *N;
+	float *pN, *pN_ref;
+	int nBufferSizeFloat;
+	int height, width, r;
+
+	void release();
+};
+
+void driverBoxFilter::readFile(float **ppBuffer, std::string filename, int& width, int& height)
+{
+	FILE* fp=fopen( filename.c_str(),"rb");
+	if(fp == NULL)
+	{
+		printf("Cann't open the file!\n");
+		exit(0);
+	}
+	fread( &width, sizeof(int), 1, fp);
+	fread( &height, sizeof(int), 1, fp);
+
+	nBufferSizeFloat = width * height * sizeof(float);
+	*ppBuffer = (float*)malloc( nBufferSizeFloat );
+
+	fread( *ppBuffer, width*height*sizeof(float), 1, fp);
+
+	fclose(fp);
+}
+
+void driverBoxFilter::writeFile(float *buffer, std::string filename, int width, int height/*=1*/)
+{
+	FILE* fp=fopen( filename.c_str(),"wb");
+	if(fp == NULL)
+	{
+		printf("Cann't open the file!\n");
+		exit(0);
+	}
+	fwrite( &width, sizeof(int), 1, fp);
+	fwrite( &height, sizeof(int), 1, fp);
+	fwrite( buffer, width*height*sizeof(float), 1, fp);
+	fclose(fp);
+}
+bool driverBoxFilter::verifyResult(float *p, float *p_ref, int n)
+{
+	for (int i=0; i<n; i++ ) 
+	{
+		if ( fabs( p[i] - p_ref[i] ) > 1e-5 )
+			return false;
+	}
+
+	return true ;
+}
+
+void driverBoxFilter::initialize( )
+{
+	//read Parameter
+	readFile( &pN_ref, "N.out", width, height );
+
+
+	pN = (float*)malloc( nBufferSizeFloat );
+
+	cudaMalloc((void**)&imCum_C,nBufferSizeFloat);
+    cudaMalloc((void**)&C_ones,nBufferSizeFloat);
+    cudaMalloc((void**)&N,nBufferSizeFloat);
+
+	thrust::fill( thrust::device_ptr<float>(C_ones), thrust::device_ptr<float>(C_ones + width*height), 1.0f);
+
+}
+
+void driverBoxFilter::release( )
+{
+	free( pN );			free( pN_ref );
+	cudaFree(imCum_C);  cudaFree(N);		cudaFree(C_ones);
+}
+
+void driverBoxFilter::run()
+{
+	boxfilter(C_ones,imCum_C,N,r,height,width);
+
+	cudaMemcpy( pN, N, nBufferSizeFloat, cudaMemcpyDeviceToHost );
+
+	verifyResult( pN, pN_ref, width*height );
+}
+
+int main(int argc, char* argv[])
+{
+
+	driverBoxFilter	test;
+	
+	test.initialize();
+	
+	test.run();
+
+	return 0;
+}
